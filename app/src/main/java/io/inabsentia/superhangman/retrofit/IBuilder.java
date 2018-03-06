d6a97f50efdf85012dca72c3f09985a210aff98d
dani972e <anusic@live.dk>
2018-03-06 11:25:56 +0100

    Adding retrofit library and creating package for it

--- app/src/main/java/io/inabsentia/superhangman/retrofit/IBuilder.java
+++ app/src/main/java/io/inabsentia/superhangman/retrofit/IBuilder.java
@@ -1,5 +1,12 @@
 package io.inabsentia.superhangman.retrofit;
 
 
+import kotlin.jvm.Throws;
+import retrofit2.http.POST;
+
 public interface IBuilder {
+
+    @POST("http://ubuntu4.javabog.dk:8080/webapp/rest/auth/login?username=s155005&password=godkode")
+
+
 }
